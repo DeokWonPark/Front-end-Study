52e3d89a40df47e3dc4a354ae1d2de7ca8ea440b
ejrdnjs <ejrdnjs96@gmail.com>
2021-02-18 00:20:05 +0900

    Add edit form

--- ReactStudy/cardmaker/src/components/maker/maker.jsx
+++ ReactStudy/cardmaker/src/components/maker/maker.jsx
@@ -1,10 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
+import Editor from '../editor/editor';
 import Footer from '../footer/footer';
 import Header from '../header/header';
+import Preview from '../preview/preview';
 import styles from "./maker.module.css"
 
 const Maker = ({authService}) => {
+    const [cards,setCards]=useState([
+        {
+            id:"1",
+            name:"ellie",
+            company:"sansung",
+            theme:"light",
+            title:"software Enginner",
+            email:"ejrdnjs96@gmail.com",
+            message: "go for it",
+            fileName:"ellie",
+            fileURL:null
+        },
+        {
+            id:"2",
+            name:"ellie",
+            company:"sansung",
+            theme:"dark",
+            title:"software Enginner",
+            email:"ejrdnjs96@gmail.com",
+            message: "go for it",
+            fileName:"ellie",
+            fileURL:null
+        },
+        {
+            id:"3",
+            name:"ellie",
+            company:"sansung",
+            theme:"colorful",
+            title:"software Enginner",
+            email:"ejrdnjs96@gmail.com",
+            message: "go for it",
+            fileName:"ellie",
+            fileURL:"ellie.png"
+        }
+    ])
     const history=useHistory();
     const onLogout=()=>{
         authService.logout();
@@ -16,9 +53,18 @@ const Maker = ({authService}) => {
                 history.push("/");
             }
         })
-    })
+    });
+
+    const addCard=(card)=>{
+        const updated=[...cards,card];
+        setCards(updated);
+    }
     return <section className={styles.maker}>
         <Header onLogout={onLogout}></Header>
+        <div className={styles.container}>
+            <Editor cards={cards} addCard={addCard}></Editor>
+            <Preview cards={cards}></Preview>
+        </div>
         <Footer></Footer>
     </section>
 }
