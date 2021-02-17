ca24b6f18c051c518f8334f313279fe5c7d75a2f
ejrdnjs <ejrdnjs96@gmail.com>
2021-02-17 16:24:45 +0900

    Revert "Revert "class cardMaker user logic""
    This reverts commit 4a9b41f9a08e24904590edea67b2acf6a264b6a8.

---
+++ ReactStudy/cardmaker/src/components/header/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './header.module.css';
+
+const Header = ({onLogout}) => {
+    return <header className={styles.header}>
+        {onLogout && (<button className={styles.logout} onClick={onLogout}>Logout</button>)}
+        <img src="/images/logo.png" alt="logo" className={styles.logo}/>
+        <h1 className={styles.title}>Business Card Maker</h1>
+    </header>
+}
+
+export default Header;
