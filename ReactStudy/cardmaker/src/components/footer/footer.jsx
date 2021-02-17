4a9b41f9a08e24904590edea67b2acf6a264b6a8
ejrdnjs <ejrdnjs96@gmail.com>
2021-02-17 16:19:30 +0900

    Revert "class cardMaker user logic"
    This reverts commit 24cfe95b3643edd794e96c140b4f80ea4bb5234c.

--- ReactStudy/cardmaker/src/components/footer/footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from './footer.module.css';
-
-const Footer = (props) => {
-    return <footer className={styles.footer}>
-        <p className={styles.title}>Code your dream</p>
-    </footer>
-}
-export default Footer;
--- ReactStudy/cardmaker/src/components/maker/maker.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Maker = (props) => {
-    return<h2>Maker</h2>
-}
-
-export default Maker;
