d10e5d85fc0237a8e6e3838eb2700ecb424fa610
ejrdnjs <ejrdnjs96@gmail.com>
2021-02-17 16:35:39 +0900

    reset history check1

--- ReactStudy/cardmaker/src/service/firebase.js
+++ ReactStudy/cardmaker/src/service/firebase.js
@@ -5,6 +5,7 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   databaseURL:process.env.REACT_APP_FIREBASE_DATABASE_URL,
 };
-// Initialize Firebase
+
+
 const firebaseApp=firebase.initializeApp(firebaseConfig);
 export default firebaseApp;
