f9accd2015cd88af5fe999229b101e279380f61e
ejrdnjs <ejrdnjs96@gmail.com>
2021-03-23 22:53:51 +0900

    Vanilla js Router 기능

---
+++ VanillaJS-Router/router.js
@@ -0,0 +1,59 @@
+// template
+
+
+const Home = `<h1>Home</h1>`
+const About = `<h1>About</h1>`
+
+const routes = {
+  '/': Home,
+  '/home': Home,
+  '/about': About
+}
+
+// entry point
+function initialRoutes (mode, el) {
+  renderHTML(el, routes['/'])
+
+  if (mode === 'history') {
+    window.onpopstate = () => renderHTML(el, routes[window.location.pathname])
+  } else {
+    window.addEventListener('hashchange', () => {
+      return renderHTML(el, getHashRoute())
+    })
+  }
+}
+
+// set browser history
+function historyRouterPush (pathName, el) {
+  window.history.pushState({}, pathName, window.location.origin + pathName)
+  renderHTML(el, routes[pathName])
+}
+
+// get hash history route
+function getHashRoute () {
+  let route = '/'
+
+  Object.keys(routes).forEach(hashRoute => {
+    if (window.location.hash.replace('#', '') === hashRoute.replace('/', '')) {
+      route = routes[hashRoute]
+    }
+  })
+
+  return route
+}
+
+// set hash history
+function hashRouterPush (pathName, el) {
+  renderHTML(el, getHashRoute())
+}
+
+// render
+function renderHTML (el, route) {
+  el.innerHTML = route
+}
+
+export {
+  initialRoutes,
+  historyRouterPush,
+  hashRouterPush
+}
