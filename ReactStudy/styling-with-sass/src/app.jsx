1892034e9eba5b2cbe511b8c7f1e7baee053b9a9
ejrdnjs <ejrdnjs96@gmail.com>
2021-03-26 01:02:13 +0900

    study Sass start

---
+++ ReactStudy/styling-with-sass/src/app.jsx
@@ -0,0 +1,14 @@
+import "./app.scss";
+import Button from "./components/button/button";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="buttons">
+        <Button>BUTTON</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
